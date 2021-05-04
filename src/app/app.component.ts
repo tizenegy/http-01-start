e10b39d0068dd2d887e859182f9bf43c196fa765
SERVERFLS\bastian.elfert <bastian.elfert@fastleansmart.com>
2021-05-04 12:34:52 +0200

    added generics and list for outputting posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
+import {Post} from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -18,17 +19,17 @@ export class AppComponent implements OnInit {
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    this.http.post('https://angular-http-389af-default-rtdb.europe-west1.firebasedatabase.app/posts.json', postData)
+    this.http.post<{name:string}>('https://angular-http-389af-default-rtdb.europe-west1.firebasedatabase.app/posts.json', postData)
       .subscribe(responseData => {
       console.log(responseData);
     });
   }
 
   private fetchPost() {
-    this.http.get('https://angular-http-389af-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
+    this.http.get<{ [key: string]: Post }>('https://angular-http-389af-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
       .pipe(
         map(responseData => {
-          const postsArray =[];
+          const postsArray: Post[] =[];
           for (const key in responseData) {
             if (responseData.hasOwnProperty(key)){
               postsArray.push({...responseData[key], id: key });
@@ -39,6 +40,7 @@ export class AppComponent implements OnInit {
       )
       .subscribe(posts => {
       console.log(posts);
+      this.loadedPosts = posts;
     });
   }
 
